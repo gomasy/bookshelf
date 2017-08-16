5cf2a792e07b485bb2feb11d09074e88c00ed6e8
Gomasy <nyan@gomasy.jp>
2017-08-16 17:29:29 +0900

    Add book test

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| Here you may define all of your model factories. Model factories give
+| you a convenient way to create models for testing and seeding your
+| database. Just tell the factory how a default model should look.
+|
+*/
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Book::class, function (Faker\Generator $faker) {
+    $user = factory(App\User::class)->create();
+
+    return [
+        'id' => $user->next_id,
+        'user_id' => $user->id,
+        'title' => $faker->title,
+        'title_ruby' => $faker->title,
+        'volume' => $faker->randomDigit,
+        'authors' => $faker->name,
+        'isbn' => $faker->isbn13(),
+        'jpno' => $faker->isbn10(),
+        'published_date' => $faker->date,
+        'ndl_url' => $faker->url,
+    ];
+});
---
+++ tests/Feature/BookTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+use App;
+
+class BookTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function testIndex()
+    {
+        $book = factory(App\Book::class)->create();
+        $user = App\User::find($book->user_id);
+
+        $response = $this->actingAs($user)
+            ->get('/list', [ 'X-Requested-With' => 'XMLHttpRequest' ]);
+        $response->assertStatus(200);
+        $response->assertJsonStructure([ 'data' => [] ]);
+    }
+
+    public function testCreate()
+    {
+        $headers = [ 'X-Requested-With' => 'XMLHttpRequest' ];
+        $user = factory(App\User::class)->create();
+
+        // success
+        $this->actingAs($user)
+            ->post('/create', [ 'code' => '9784873115382' ], $headers)
+            ->assertStatus(200);
+
+        // not found
+        $this->actingAs($user)
+            ->post('/create', [ 'code' => '0000000000000' ], $headers)
+            ->assertStatus(404);
+
+        // invalid
+        $this->actingAs($user)
+            ->post('/create', [ 'code' => '114514' ], $headers)
+            ->assertStatus(422);
+    }
+
+    public function testDelete()
+    {
+        $headers = [ 'X-Requested-With' => 'XMLHttpRequest' ];
+        $book = factory(App\Book::class)->create();
+        $user = App\User::find($book->user_id);
+
+        // success
+        $this->actingAs($user)
+            ->post('/delete', [ 'id' => $book->id + 1 ], $headers)
+            ->assertStatus(200);
+
+        // not found
+        $this->actingAs($user)
+            ->post('/delete', [ 'id' => '0' ], $headers)
+            ->assertStatus(404);
+
+        // invalid
+        $this->actingAs($user)
+            ->post('/delete', [ 'id' => 'chimpo' ], $headers)
+            ->assertStatus(422);
+    }
+}

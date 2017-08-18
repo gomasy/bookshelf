cbe8234c9dfff208e7300e8ab9ab278fab2484d9
Gomasy <nyan@gomasy.jp>
2017-08-18 18:54:03 +0900

    Add account/delete test

--- tests/Feature/AccountTest.php
+++ tests/Feature/AccountTest.php
@@ -16,12 +16,14 @@ class AccountTest extends TestCase
     public function testBasicTest()
     {
         $this->get('/account')->assertRedirect('/login');
+        $this->get('/account/delete')->assertRedirect('/login');
     }
 
     public function testLoggedIn()
     {
         $user = factory(App\User::class)->create();
         $this->actingAs($user)->get('/account')->assertStatus(200);
+        $this->actingAs($user)->get('/account/delete')->assertStatus(200);
     }
 
     public function testUpdate()
@@ -39,6 +41,29 @@ class AccountTest extends TestCase
         // invaild
         $this->actingAs($user)
             ->post('/account/update', [ 'email' => '' ], $headers)
+            ->assertStatus(422);
+    }
+
+    public function testDelete()
+    {
+        $headers = [ 'X-Requested-With' => 'XMLHttpRequest' ];
+        $password = 'testpasswd';
+
+        // success
+        $user = factory(App\User::class)->create([ 'password' => bcrypt($password) ]);
+        $this->actingAs($user)
+            ->post('/account/delete', [ 'password' => $password ], $headers)
+            ->assertRedirect('/');
+
+        // fail
+        $user = factory(App\User::class)->create();
+        $this->actingAs($user)
+            ->post('/account/delete', [ 'password' => $password ], $headers)
+            ->assertStatus(200);
+
+        // invaild
+        $this->actingAs($user)
+            ->post('/account/delete', [ 'password' => '' ], $headers)
             ->assertStatus(422);
     }
 }

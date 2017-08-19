80d7cb41fe97a7e4e99b85543535e25b4aece392
Gomasy <nyan@gomasy.jp>
2017-08-20 02:18:06 +0900

    Unify syntax:rocket:

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
-const webpack = require("webpack");
-const path = require("path");
+const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
-    entry: "./resources/assets/app.js",
+    entry: './resources/assets/app.js',
     output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname + "/public"),
+        filename: 'bundle.js',
+        path: path.resolve(__dirname + '/public'),
     },
     module: {
         rules: [
             {
                 test: /\.scss$/,
-                use: [ "style-loader", "css-loader", "sass-loader" ],
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ],
             },
             {
                 test: /\.(woff2?|ttf|eot|svg)(\?v=[\d.]+|\?[\s\S]+)?$/,
                 use: [
-                    { loader: "file-loader?name=/[name].[ext]" },
+                    { loader: 'file-loader?name=/[name].[ext]' },
                 ],
             },
         ],
@@ -24,10 +24,10 @@ module.exports = {
     plugins: [
         new webpack.optimize.UglifyJsPlugin(),
         new webpack.ProvidePlugin({
-            $: "jquery",
-            jquery: "jquery",
-            jQuery: "jquery",
-            "window.jQuery": "jquery",
+            $: 'jquery',
+            jquery: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
         }),
     ],
 };

94affa74850dcb0a0b7d2fa84edf3a7fa27f8f4b
Gomasy <nyan@gomasy.jp>
2017-08-22 14:12:51 +0900

    Unify syntax:rocket::rocket:

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -5,54 +5,54 @@ $(document).ready(function() {
 
         if (lang != 'en') {
             $.extend($.fn.dataTable.defaults, {
-                'language': { 'url': $messages['datatables']['url'] },
+                language: { url: $messages['datatables']['url'] },
             });
         }
 
         $table = $('#main').DataTable({
-            'columns': [
-                { 'data': 'title' },
-                { 'data': 'volume' },
-                { 'data': 'authors' },
-                { 'data': 'isbn' },
-                { 'data': 'jpno' },
-                { 'data': 'published_date' },
-                { 'data': 'ndl_url' },
+            columns: [
+                { data: 'title' },
+                { data: 'volume' },
+                { data: 'authors' },
+                { data: 'isbn' },
+                { data: 'jpno' },
+                { data: 'published_date' },
+                { data: 'ndl_url' },
             ],
-            'columnDefs': [
-                { 'visible': false, 'targets': 3 },
-                { 'visible': false, 'targets': 4 },
-                { 'visible': false, 'targets': 6 },
+            columnDefs: [
+                { visible: false, targets: 3 },
+                { visible: false, targets: 4 },
+                { visible: false, targets: 6 },
             ],
-            'order': [[ 0, 'asc' ], [ 5, 'asc' ]],
-            'lengthMenu': [ 10, 20, 30, 50, 100, 200 ],
-            'displayLength': 100,
-            'scrollY': true,
-            'deferRender': true,
-            'ajax': 'list',
-            'rowCallback': function(row, data, index) {
+            order: [[ 0, 'asc' ], [ 5, 'asc' ]],
+            lengthMenu: [ 10, 20, 30, 50, 100, 200 ],
+            displayLength: 100,
+            scrollY: true,
+            deferRender: true,
+            ajax: 'list',
+            rowCallback: function(row, data, index) {
                 $('td:eq(0)', row).html('<a href="' + data.ndl_url + '" title="' + $messages.rowsAlt + '">' + data.title + '</a>');
             },
-            'drawCallback': function(settings) {
+            drawCallback: function(settings) {
                 $('.pagination').append('<li class="paginate_button disabled" id="main_delete"><a href="#" id="delete" aria-controls="main">' + $messages.delete.label + '</a></li>');
                 $('#delete').on('click', function() {
                     if (!($('#main_delete').hasClass('disabled')) && window.confirm($messages.confirm)) {
                         var bookId = $table.row('.selected').data().id;
 
                         $.ajax({
-                            'url': 'delete',
-                            'type': 'POST',
-                            'data': {
-                                'id': bookId,
-                                '_token': document.head.querySelector('meta[name="csrf-token"]').content,
+                            url: 'delete',
+                            type: 'POST',
+                            data: {
+                                id: bookId,
+                                _token: document.head.querySelector('meta[name="csrf-token"]').content,
                             },
-                            'success': function(result) {
+                            success: function(result) {
                                 $table.row('.selected').remove().draw(false);
-                                $.notify($messages.delete.success, { 'type': 'success' });
+                                $.notify($messages.delete.success, { type: 'success' });
                             },
-                            'error': function(result) {
+                            error: function(result) {
                                 var f = {
-                                    404: function() { $.notify($messages.delete.failure, { 'type': 'danger' }); },
+                                    404: function() { $.notify($messages.delete.failure, { type: 'danger' }); },
                                     422: function() { validateError(result.responseJSON.id[0]); },
                                 };
                                 f[result.status]();
@@ -67,11 +67,11 @@ $(document).ready(function() {
     });
 
     $.notifyDefaults({
-        'placement': {
-            'from': 'bottom',
-            'align': 'right',
+        placement: {
+            from: 'bottom',
+            align: 'right',
         },
-        'mouse_over': 'pause',
+        mouse_over: 'pause',
     });
 
     $('#main tbody').on('click', 'tr', function() {
@@ -93,18 +93,18 @@ $(document).ready(function() {
 
         var $form = $(this);
         $.ajax({
-            'url': $form.attr('action'),
-            'type': $form.attr('method'),
-            'data': $form.serialize(),
-            'success': function(result) {
-                $.notify($messages.add.success, { 'type': 'success' });
+            url: $form.attr('action'),
+            type: $form.attr('method'),
+            data: $form.serialize(),
+            success: function(result) {
+                $.notify($messages.add.success, { type: 'success' });
                 $table.ajax.reload(null, false);
                 $form[0].reset();
             },
-            'error': function(result) {
+            error: function(result) {
                 var f = {
-                    404: function() { $.notify($messages.not_exist, { 'type': 'warning' }); },
-                    409: function() { $.notify($messages.add.failure, { 'type': 'danger' }); },
+                    404: function() { $.notify($messages.not_exist, { type: 'warning' }); },
+                    409: function() { $.notify($messages.add.failure, { type: 'danger' }); },
                     422: function() { validateError(result.responseJSON.code[0]); },
                 };
                 f[result.status]();
@@ -121,5 +121,5 @@ $(document).ready(function() {
 function validateError(message) {
     var $mes = $messages.invalid;
     $mes.message = '<p>' + message + '</p>';
-    $.notify($mes, { 'type': 'warning' });
+    $.notify($mes, { type: 'warning' });
 }

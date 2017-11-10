96f35e2d0fb36064a8c845dd7c2c72e67fdd8c87
Gomasy <nyan@gomasy.jp>
2017-11-10 20:02:01 +0900

    Fix account forms design

--- resources/views/account/delete.blade.php
+++ resources/views/account/delete.blade.php
@@ -3,28 +3,23 @@
 @section('content')
             <div class="container">
                 <div class="row">
-                    <div class="col-md-8 col-md-offset-2">
+                    <div class="col-md-6 col-md-offset-3">
                         <div class="panel panel-default">
                             <div class="panel-heading">{{ __('account.delete.header') }}</div>
                             <div class="panel-body">
-                                <form class="form-horizontal" role="form" action="{{ route('account/delete') }}" method="POST">
+                                <form role="form" action="{{ route('account/delete') }}" method="POST">
                                     <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                        <label class="col-md-4 control-label">{{ __('validation.attributes.password') }}</label>
-                                        <div class="col-md-6">
-                                            <input class="form-control" type="password" name="password" value="" required>
+                                        <label for="password">{{ __('validation.attributes.password') }}</label>
+                                        <input class="form-control" type="password" name="password" required autofocus>
 @if ($errors->has('password'))
-                                            <span class="help-block">
-                                                <strong>{{ $errors->first('password') }}</strong>
-                                            </span>
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('password') }}</strong>
+                                        </span>
 @endif
-                                        </div>
                                     </div>
-                                    <div class="form-group">
-                                        <div class="col-md-6 col-md-offset-4">
-                                            {{ csrf_field() }}
-                                            <button type="submit" class="btn btn-danger">{{ __('account.delete.submit') }}</button>
-                                        </div>
-                                    </div>
+                                    <hr>
+                                    {{ csrf_field() }}
+                                    <button type="submit" class="btn btn-block btn-danger">{{ __('account.delete.submit') }}</button>
                                 </form>
                             </div>
                         </body>
--- resources/views/account/update.blade.php
+++ resources/views/account/update.blade.php
@@ -3,60 +3,46 @@
 @section('content')
             <div class="container">
                 <div class="row">
-                    <div class="col-md-8 col-md-offset-2">
+                    <div class="col-md-6 col-md-offset-3">
                         <div class="panel panel-default">
                             <div class="panel-heading">{{ __('account.update.header') }}</div>
                             <div class="panel-body">
-                                <form class="form-horizontal" role="form" action="{{ route('account/update') }}" method="POST">
+                                <form role="form" action="{{ route('account/update') }}" method="POST">
                                     <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                        <label class="col-md-4 control-label">{{ __('validation.attributes.email') }}</label>
-                                        <div class="col-md-6">
-                                            <input class="form-control" type="email" name="email" value="{{ old('email', Auth::user()['email']) }}">
+                                        <label for="email">{{ __('validation.attributes.email') }}</label>
+                                        <input class="form-control" type="email" name="email" value="{{ old('email', Auth::user()['email']) }}">
 @if ($errors->has('email'))
-                                            <span class="help-block">
-                                                <strong>{{ $errors->first('email') }}</strong>
-                                            </span>
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
 @endif
-                                        </div>
                                     </div>
                                     <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                                        <label class="col-md-4 control-label">{{ __('validation.attributes.name') }}</label>
-                                        <div class="col-md-6">
-                                            <input class="form-control" type="text" name="name" value="{{ old('name', Auth::user()['name']) }}">
+                                        <label for="name">{{ __('validation.attributes.name') }}</label>
+                                        <input class="form-control" type="text" name="name" value="{{ old('name', Auth::user()['name']) }}">
 @if ($errors->has('name'))
-                                            <span class="help-block">
-                                                <strong>{{ $errors->first('name') }}</strong>
-                                            </span>
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('name') }}</strong>
+                                        </span>
 @endif
-                                        </div>
                                     </div>
                                     <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                        <label class="col-md-4 control-label">{{ __('validation.attributes.password') }}</label>
-                                        <div class="col-md-6">
-                                            <input class="form-control" type="password" name="password" value="">
+                                        <label for="password">{{ __('validation.attributes.password') }}</label>
+                                        <input class="form-control" type="password" name="password" value="">
 @if ($errors->has('password'))
-                                            <span class="help-block">
-                                                <strong>{{ $errors->first('password') }}</strong>
-                                            </span>
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('password') }}</strong>
+                                        </span>
 @endif
-                                        </div>
                                     </div>
                                     <div class="form-group">
-                                        <label class="col-md-4 control-label">{{ __('validation.attributes.password-confirm') }}</label>
-                                        <div class="col-md-6">
-                                            <input class="form-control" type="password" name="password_confirmation" value="">
-                                        </div>
+                                        <label for="password_confirmation">{{ __('validation.attributes.password-confirm') }}</label>
+                                        <input class="form-control" type="password" name="password_confirmation" value="">
                                     </div>
-                                    <div class="form-group">
-                                        <div class="col-md-6 col-md-offset-4">
-                                            {{ csrf_field() }}
-                                            <button type="submit" class="btn btn-primary">{{ __('account.update.submit') }}</button>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="col-md-6 col-md-offset-4">
-                                            <a href="{{ route('account/delete') }}">{{ __('account.delete.link') }}</a>
-                                        </div>
+                                    {{ csrf_field() }}
+                                    <button type="submit" class="btn btn-block btn-primary">{{ __('account.update.submit') }}</button>
+                                    <hr>
+                                    <p><a href="{{ route('account/delete') }}">{{ __('account.delete.link') }}</a></p>
                                     </div>
                                 </form>
                             </div>

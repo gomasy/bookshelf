f87c089f01bc83857fe4f70920bdadb5af67527e
Gomasy <nyan@gomasy.jp>
2017-12-11 06:53:00 +0900

    Remove unnecessary parts of bundle.js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,7 +6,6 @@ import 'datatables.net-bs';
 import './main.js';
 
 // scss
-import 'font-awesome/scss/font-awesome.scss';
 import '../scss/bootstrap/bootstrap.scss';
 import '../scss/datatables.scss';
 import '../scss/app.scss';

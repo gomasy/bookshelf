fec5ce41c50469d68578c014aa44d3d0e829cc17
Gomasy <nyan@gomasy.jp>
2018-01-04 04:03:29 +0900

    Remove initial-scale=1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width">
 @if (!App::isDownForMaintenance())
         <meta name="csrf-token" content="{{ csrf_token() }}">
 @endif

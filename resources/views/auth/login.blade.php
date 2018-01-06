6e798f3616ec45a73778d826d4523b9dd9e5a5f1
Gomasy <nyan@gomasy.jp>
2018-01-07 04:06:25 +0900

    Fix indents

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,44 +3,44 @@
 @section('title', __('auth.sign.in'))
 
 @section('content')
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6 col-md-offset-3">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">{{ __('auth.sign.in') }}</div>
-                            <div class="panel-body">
-                                <form role="form" method="POST" action="{{ route('login') }}">
-                                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                        <label for="email">{{ __('validation.attributes.email') }}</label>
-                                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="example@example.com" required autofocus>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <div class="panel panel-default">
+                <div class="panel-heading">{{ __('auth.sign.in') }}</div>
+                <div class="panel-body">
+                    <form role="form" method="POST" action="{{ route('login') }}">
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email">{{ __('validation.attributes.email') }}</label>
+                            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="example@example.com" required autofocus>
 @if ($errors->has('email'))
-                                        <span class="help-block">
-                                            <strong>{{ $errors->first('email') }}</strong>
-                                        </span>
+                            <span class="help-block">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
 @endif
-                                    </div>
-                                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                        <label for="password">{{ __('validation.attributes.password') }}</label>
-                                        <input id="password" type="password" class="form-control" name="password" placeholder="●●●●●●●●" required>
-@if ($errors->has('password'))
-                                        <span class="help-block">
-                                            <strong>{{ $errors->first('password') }}</strong>
-                                        </span>
-@endif
-                                    </div>
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox"> {{ __('auth.sign.remember') }}
-                                        </label>
-                                    </div>
-                                    {{ csrf_field() }}
-                                    <button type="submit" class="btn btn-block btn-primary">{{ __('auth.sign.in') }}</button>
-                                </form>
-                                <hr>
-                                <p><a href="{{ route('password.request') }}">{{ __('passwords.forgot') }}</a></p>
-                            </div>
                         </div>
-                    </div>
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password">{{ __('validation.attributes.password') }}</label>
+                            <input id="password" type="password" class="form-control" name="password" placeholder="●●●●●●●●" required>
+@if ($errors->has('password'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
+@endif
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox"> {{ __('auth.sign.remember') }}
+                            </label>
+                        </div>
+                        {{ csrf_field() }}
+                        <button type="submit" class="btn btn-block btn-primary">{{ __('auth.sign.in') }}</button>
+                    </form>
+                    <hr>
+                    <p><a href="{{ route('password.request') }}">{{ __('passwords.forgot') }}</a></p>
                 </div>
             </div>
+        </div>
+    </div>
+</div>
 @endsection
--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-            <div class="container">
-                <div class="row">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">Service unavailable</div>
-                        <div class="panel-body">
-                            {{ json_decode(file_get_contents(storage_path('framework/down')), true)['message'] }}
-                        </div>
-                    </div>
-                </div>
+<div class="container">
+    <div class="row">
+        <div class="panel panel-default">
+            <div class="panel-heading">Service unavailable</div>
+            <div class="panel-body">
+                {{ json_decode(file_get_contents(storage_path('framework/down')), true)['message'] }}
             </div>
+        </div>
+    </div>
+</div>
 @endsection

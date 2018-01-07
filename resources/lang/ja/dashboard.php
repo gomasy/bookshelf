f6f2f20846798941749bcaf7b7c3fb909e351bfc
Gomasy <nyan@gomasy.jp>
2018-01-07 21:59:57 +0900

    Fix copyright notice

--- resources/lang/ja/dashboard.php
+++ resources/lang/ja/dashboard.php
@@ -13,6 +13,6 @@ return [
     'credits' => [
         'このサービスは<a href="http://iss.ndl.go.jp/information/outline/" target="_blank">国立国会図書館サーチ</a>の外部提供インターフェイスを利用しています。',
         '登録される情報の正確性については責任を負いかねます。',
-        '©2017 Gomasy.',
+        '©2017-2018 Gomasy.',
     ],
 ];

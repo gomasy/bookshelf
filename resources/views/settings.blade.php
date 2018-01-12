e4a1b4d8f1aa7447f7dc0a5a6343ced2ce72ae03
Gomasy <nyan@gomasy.jp>
2018-01-12 18:00:04 +0900

    Fix indent

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -5,17 +5,17 @@
 @endsection
 
 @section('content')
-    <div class="row" id="settings">
-        <div class="col-lg-2 col-sm-3">
-            <div class="panel panel-default">
-                <div class="panel-heading">{{ __('settings.title') }}</div>
-                <ul class="list-unstyled" id="settings-menu">
-                    <li{!! Request::path() === 'settings/account' ? ' class="active"' : '' !!}><a href="/settings/account"><i class="glyphicon glyphicon-user"></i>{{ __('settings.account.update.title') }}</a></li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-lg-6 col-sm-7">
-            @yield('account')
+<div class="row" id="settings">
+    <div class="col-lg-2 col-sm-3">
+        <div class="panel panel-default">
+            <div class="panel-heading">{{ __('settings.title') }}</div>
+            <ul class="list-unstyled" id="settings-menu">
+                <li{!! Request::path() === 'settings/account' ? ' class="active"' : '' !!}><a href="/settings/account"><i class="glyphicon glyphicon-user"></i>{{ __('settings.account.update.title') }}</a></li>
+            </ul>
         </div>
     </div>
+    <div class="col-lg-6 col-sm-7">
+        @yield('account')
+    </div>
+</div>
 @endsection

d8ed0bcf2b3285b449e038f95ca82ee32e009ce8
Gomasy <nyan@gomasy.jp>
2018-05-03 19:45:47 +0900

    font-awesome 再導入

--- resources/assets/js/core.js
+++ resources/assets/js/core.js
@@ -1,5 +1,6 @@
 import 'bootstrap-sass';
 import 'bootstrap-sass/assets/stylesheets/_bootstrap.scss';
+import 'font-awesome/scss/font-awesome.scss';
 
 import '../scss/core.scss';
 import '../icon.png';
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,17 +13,17 @@
     <hr>
     <div class="row">
         <div class="col-sm-4">
-            <i class="glyphicon glyphicon-barcode" aria-hidden="true"></i>
+            <i class="fa fa-barcode" aria-hidden="true"></i>
             <h3>{{ __('home.row.barcode.title') }}</h3>
             <p>{{ __('home.row.barcode.text') }}</p>
         </div>
         <div class="col-sm-4">
-            <i class="glyphicon glyphicon-list" aria-hidden="true"></i>
+            <i class="fa fa-list" aria-hidden="true"></i>
             <h3>{{ __('home.row.list.title') }}</h3>
             <p>{{ __('home.row.list.text') }}</p>
         </div>
         <div class="col-sm-4">
-            <i class="glyphicon glyphicon-globe" aria-hidden="true"></i>
+            <i class="fa fa-github" aria-hidden="true"></i>
             <h3>{{ __('home.row.opensource.title') }}</h3>
             <p>{{ __('home.row.opensource.text') }}</p>
         </div>

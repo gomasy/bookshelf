d492e2dc90a670465f8a0a11174299eb68e93e87
Gomasy <nyan@gomasy.jp>
2018-06-24 23:58:39 +0900

    #11 対応（未確認）

--- resources/assets/js/general.js
+++ resources/assets/js/general.js
@@ -2,5 +2,9 @@ import 'bootstrap-sass';
 import '../scss/vendor/_bootstrap.scss';
 import 'font-awesome/scss/font-awesome.scss';
 
+// polyfill
+import 'babel-polyfill';
+
+// assets
 import '../scss/general.scss';
 import '../icon.png';

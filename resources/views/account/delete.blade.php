812ae2b7949c4416f9bc7a88d6ab14a1b5d67b36
Gomasy <nyan@gomasy.jp>
2018-10-06 10:13:32 +0900

    アカウント削除画面のレイアウト崩れを修正

--- resources/views/account/delete.blade.php
+++ resources/views/account/delete.blade.php
@@ -7,27 +7,29 @@
 @endsection
 
 @section('content')
-<div class="row">
-    <div class="col-md-6 col-md-offset-3">
-        <div class="panel panel-danger">
-            <div class="panel-heading">{{ __('settings.account.delete.header') }}</div>
-            <div class="panel-body">
-                <form role="form" method="POST" action="/settings/account/delete">
-                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                        <label for="password">{{ __('validation.attributes.password') }}</label>
-                        <input class="form-control" type="password" name="password" required autofocus>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <div class="panel panel-danger">
+                <div class="panel-heading">{{ __('settings.account.delete.header') }}</div>
+                <div class="panel-body">
+                    <form role="form" method="POST" action="/settings/account/delete">
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password">{{ __('validation.attributes.password') }}</label>
+                            <input class="form-control" type="password" name="password" required autofocus>
 @if ($errors->has('password'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('password') }}</strong>
-                        </span>
+                            <span class="help-block">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
 @endif
-                    </div>
-                    <hr>
-                    {{ csrf_field() }}
-                    <button type="submit" class="btn btn-block btn-danger">{{ __('settings.account.delete.submit') }}</button>
-                </form>
-            </div>
-        </body>
+                        </div>
+                        <hr>
+                        {{ csrf_field() }}
+                        <button type="submit" class="btn btn-block btn-danger">{{ __('settings.account.delete.submit') }}</button>
+                    </form>
+                </div>
+            </body>
+        </div>
     </div>
 </div>
 @endsection

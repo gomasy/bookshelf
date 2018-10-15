gomasy/bookshelf

commit 84cb3964cc610f0a4e0e0909bf329504ec01e457

メールアドレスの認証を追加 (#12)

routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', 'HomeController@index');
+Route::get('/', 'HomeController@index')->middleware('verified');
 Route::get('/list.json', 'BookController@list');
 Route::get('/create', 'BookController@scanner');
 Route::post('/create', 'BookController@create');
@@ -28,4 +28,4 @@ Route::group([ 'prefix' => 'settings' ], function () {
     });
 });
 
-Auth::routes();
+Auth::routes([ 'verify' => true ]);

app/User.php
@@ -8,9 +8,9 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmailContract
 {
-    use Notifiable;
+    use MustVerifyEmail, Notifiable;
 
     /**
      * The attributes that are mass assignable.

resources/lang/ja/auth.php
@@ -24,4 +24,10 @@ return [
         'out' => 'ログアウト',
         'remember' => 'ログイン状態を記憶',
     ],
+    'verify' => [
+        'title' => 'メールアドレスの確認',
+        'header' => 'メールアドレスの確認が必要です',
+        'message' => '確認リンクが入力されたメールアドレスに送信されました。<br>メールが届かない場合は下記ボタンをクリックして、別のメールをリクエストしてください。',
+        'resend' => '再送信',
+    ],
 ];

resources/views/auth/verify.blade.php
@@ -1,0 +1,25 @@
+@extends('layouts.app')
+
+@section('title', __('auth.verify.title'))
+
+@section('head')
+<script src="@asset('/assets/general.min.js')"></script>
+@endsection
+
+@section('content')
+<div class="row">
+    <div class="col-md-6 col-md-offset-3">
+        <div class="panel panel-info">
+            <div class="panel-heading">{{ __('auth.verify.header') }}</div>
+            <div class="panel-body">
+                <p>{!! __('auth.verify.message') !!}</p>
+                <div class="row">
+                    <div class="col-md-2">
+                        <a class="btn btn-block btn-primary" href="{{ route('verification.resend') }}">{{ __('auth.verify.resend') }}</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection

commit 73aab820455a1b246a200dc04c4134d5be698738

ホーム画面が表示されない問題を修正 (caused by 84cb3964cc610f0a4e0e0909bf329504ec01e457)

app/Http/Controllers/Auth/VerificationController.php
@@ -25,7 +25,7 @@ class VerificationController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.

app/Http/Controllers/HomeController.php
@@ -13,6 +13,14 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {
-        return \Auth::check() ? view('dashboard') : view('home');
+        if (\Auth::check()) {
+            if (\Auth::user()->hasVerifiedEmail()) {
+                return view('dashboard');
+            } else {
+                return redirect('email/verify');
+            }
+        } else {
+            return view('home');
+        }
     }
 }

routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', 'HomeController@index')->middleware('verified');
+Route::get('/', 'HomeController@index');
 Route::get('/list.json', 'BookController@list');
 Route::get('/create', 'BookController@scanner');
 Route::post('/create', 'BookController@create');
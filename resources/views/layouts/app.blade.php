gomasy/bookshelf

commit 0c2f0ea27aabbd04846ba1feac13d40bfe33f23f

やっぱりエントリポイントを分けた

resources/views/layouts/app.blade.php
@@ -11,6 +11,8 @@
         <title>{{ config('app.name') }} - @yield('title')</title>
         <link href="@asset('/assets/icon.png')" rel="icon" type="image/png">
         <script defer src="@asset('/assets/app.js')"></script>
+        @yield('head')
+
         <!-- ogp -->
         <meta property="og:title" content="{{ config('app.name') }} - @yield('title')">
         <meta property="og:type" content="{{ Request::path() === '/' ? 'website' : 'article' }}">

webpack.config.js
@@ -1,12 +1,17 @@
+const glob = require('glob');
 const path = require('path');
 const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
+const entries = {};
+glob.sync('./resources/assets/js/*.js').map(entry => {
+    const name = entry.match('^.+/(.+?)\\.js$')[1];
+    entries[name] = entry;
+});
+
 module.exports = {
     mode: process.env.NODE_ENV,
-    entry: {
-        app: './resources/assets/js/app.js',
-    },
+    entry: entries,
     output: {
         path: path.join(__dirname, '/public'),
         filename: './assets/[name].js',

resources/assets/js/app.js
@@ -8,23 +8,5 @@ import 'babel-polyfill';
 // assets
 import '../icon.png';
 import '../scss/app.scss';
-import '../scss/dashboard.scss';
 import '../scss/home.scss';
 import '../scss/settings.scss';
-
-// js
-import dashboard from './dashboard.js';
-
-const options = {
-    ajax: {
-        'Content-Type': 'application/json',
-        'X-Csrf-Token': document.head.querySelector('meta[name="csrf-token"]').content,
-        'X-Requested-With': 'XMLHttpRequest',
-    },
-};
-
-switch (window.location.pathname) {
-case '/':
-    dashboard(options);
-    break;
-}

resources/assets/js/dashboard.js
@@ -2,19 +2,23 @@ import Vue from 'vue';
 import Notifications from 'vue-notification';
 import Datatable from 'vue2-datatable-component';
 
-// components
+Vue.use(Notifications);
+Vue.use(Datatable);
+
+import '../scss/dashboard.scss';
 import tablePanel from './components/tablePanel.vue';
 
-export default function(options) {
-    Vue.use(Notifications);
-    Vue.use(Datatable);
-
-    return new Vue({
-        el: '#content',
-        components: { tablePanel },
-        template: '<tablePanel :options="options" />',
-        data: () => ({
-            options: options,
-        }),
-    });
-}
+new Vue({
+    el: '#content',
+    components: { tablePanel },
+    template: '<tablePanel :options="options" />',
+    data: () => ({
+        options: {
+            ajax: {
+                'Content-Type': 'application/json',
+                'X-Csrf-Token': document.head.querySelector('meta[name="csrf-token"]').content,
+                'X-Requested-With': 'XMLHttpRequest',
+            },
+        },
+    }),
+});

resources/views/dashboard.blade.php
@@ -2,6 +2,10 @@
 
 @section('title', 'ダッシュボード')
 
+@section('head')
+<script defer src="@asset('/assets/dashboard.js')"></script>
+@endsection
+
 @section('navbar')
 <div id="register"></div>
 @endsection

commit 9fc35a87733018156df79421a1513c776176829a

ライブラリの重複読込を防止 (caused by 0c2f0ea27aabbd04846ba1feac13d40bfe33f23f)

resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
         <meta property="description" content="手持ちの書籍をISBNコードなどで管理し、ブラウザ上で簡単に表示・検索できるサービスです">
         <title>{{ config('app.name') }} - @yield('title')</title>
         <link href="@asset('/assets/icon.png')" rel="icon" type="image/png">
+        <script src="@asset('/assets/vendor.js')"></script>
         <script defer src="@asset('/assets/app.js')"></script>
         @yield('head')
 

webpack.config.js
@@ -48,6 +48,12 @@ module.exports = {
             },
         ],
     },
+    optimization: {
+        splitChunks: {
+            name: 'vendor',
+            chunks: 'initial',
+        },
+    },
     plugins: [
         new webpack.ProvidePlugin({
             $: 'jquery',
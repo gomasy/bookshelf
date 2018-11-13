558ad6486dce154b07c95d2aac0b0c3ba7739352
Gomasy <nyan@gomasy.jp>
2018-11-13 10:42:35 +0900

    各サイズごとの NO IMAGE 画像を作成

--- app/Libs/AmazonImages.php
+++ app/Libs/AmazonImages.php
@@ -6,13 +6,19 @@ namespace App\Libs;
 class AmazonImages
 {
     protected $endpoint = 'http://images-jp.amazon.com';
-    protected $path = '/images/P/';
+    protected $path = 'images/P/';
     protected $countryCode = '09';
     protected $types = [
         'thumb' => 'THUMBZZZ',
         'small' => 'TZZZZZZZ',
         'medium' => 'MZZZZZZZ',
         'large' => 'LZZZZZZZ',
+    ];
+    protected $sizes = [
+        'thumb' => [ [ 52, 75 ], [ 2, 3, 28 ] ],
+        'small' => [ [ 77, 110 ], [ 3, 11, 45 ] ],
+        'medium' => [ [ 112, 160 ], [ 5, 22, 70 ] ],
+        'large' => [ [ 349, 500 ], [ 5, 140, 235 ] ],
     ];
 
     public function all($isbn10, $endpoint = null): array
@@ -32,34 +38,34 @@ class AmazonImages
         }
 
         if ($isbn10 === null) {
-            return "{$endpoint}{$this->path}missing.jpg";
+            return "{$endpoint}/{$this->path}missing.{$type}.jpg";
         }
 
-        return "{$endpoint}{$this->path}{$isbn10}.{$this->countryCode}.{$this->types[$type]}";
+        return "{$endpoint}/{$this->path}{$isbn10}.{$this->countryCode}.{$this->types[$type]}";
     }
 
     public function fetch($path): ?string
     {
-        if ($path !== "{$this->path}missing.jpg") {
-            $image = file_get_contents("{$this->endpoint}/{$path}");
-            $size = getimagesizefromstring($image);
-
-            if ($size[0] <= 1 || $size[1] <= 1) {
-                return $this->missing();
-            }
-
-            return $image;
+        if (preg_match('/^' . preg_quote($this->path, '/') . 'missing\.(.+)\.jpg$/', $path, $matches)) {
+            return $this->missing($this->sizes[$matches[1]]);
         }
 
-        return $this->missing();
+        $image = file_get_contents("{$this->endpoint}/{$path}");
+        $size = getimagesizefromstring($image);
+
+        if ($size[0] <= 1 || $size[1] <= 1) {
+            return $this->missing();
+        }
+
+        return $image;
     }
 
-    protected function missing()
+    protected function missing($size = null)
     {
-        $image = imagecreate(112, 160);
+        $image = imagecreate($size[0][0], $size[0][1]);
         imagecolorallocate($image, 200, 200, 200);
         $text_color = imagecolorallocate($image, 100, 100, 100);
-        imagestring($image, 5, 22, 70, 'NO IMAGE', $text_color);
+        imagestring($image, $size[1][0], $size[1][1], $size[1][2], 'NO IMAGE', $text_color);
         imagejpeg($image);
 
         return null;

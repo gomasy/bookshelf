gomasy/bookshelf

commit 8d5008559cacd2e6a617e6f6d7e9e4c25ff20f96

出版日、価格のカラムを追加 (close #69)

database/migrations/2018_11_14_185849_create_publisher_and_price_column.php
@@ -1,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+use Facades\App\Libs\NDL;
+
+class CreatePublisherAndPriceColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->string('publisher')->nullable()->after('jpno');
+            $table->string('price')->nullable()->after('publisher');
+        });
+
+        \DB::transaction(function () {
+            foreach (\DB::table('books')->get() as $book) {
+                if ($book->isbn !== null) {
+                    $query = NDL::query($book->isbn);
+                    \DB::table('books')->where('id', $book->id)
+                        ->where('user_id', $book->user_id)
+                        ->update([
+                            'publisher' => $query['publisher'],
+                            'price' => $query['price'],
+                        ]);
+                    echo "Updated: {$book->user_id} -> {$book->title}\n";
+                }
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->dropColumn('publisher');
+            $table->dropColumn('price');
+        });
+    }
+}

app/Book.php
@@ -15,7 +15,7 @@ class Book extends Model
      *
      * @var array
      */
-    protected $fillable = [ 'id', 'user_id', 'title', 'volume', 'authors', 'isbn', 'jpno', 'ndl_url' ];
+    protected $fillable = [ 'id', 'user_id', 'title', 'volume', 'authors', 'isbn', 'jpno', 'publisher', 'price', 'ndl_url' ];
 
     /**
      * The accessors to append to the model's array form.

app/Libs/NDL.php
@@ -31,12 +31,14 @@ class NDL
         }
 
         return [
-            'title'   => $this->getTitle(),
-            'volume'  => $this->getVolume(),
-            'authors' => $this->getAuthors(),
-            'isbn'    => $this->getISBN(),
-            'jpno'    => $this->getJPNO(),
-            'ndl_url' => $this->getBookUrl(),
+            'title'     => $this->getTitle(),
+            'volume'    => $this->getVolume(),
+            'authors'   => $this->getAuthors(),
+            'isbn'      => $this->getISBN(),
+            'jpno'      => $this->getJPNO(),
+            'publisher' => $this->getPublisher(),
+            'price'     => $this->getPrice(),
+            'ndl_url'   => $this->getBookUrl(),
         ];
     }
 
@@ -126,6 +128,16 @@ class NDL
         $authors = rtrim((string)$this->obj->author, ',');
 
         return preg_match($this->regexp[1], $authors, $str) ? $str[1] : $authors;
+    }
+
+    public function getPublisher(): ?string
+    {
+        return (string)$this->obj->publisher;
+    }
+
+    public function getPrice(): ?string
+    {
+        return (string)$this->obj->price;
     }
 
     public function getBookUrl(): string

commit 5db9f008de24b7bb5196db76bb9fbf62953628c8

データ補完処理中に落ちる問題を修正 (caused by 8d5008559cacd2e6a617e6f6d7e9e4c25ff20f96)

database/migrations/2018_11_14_185849_create_publisher_and_price_column.php
@@ -27,8 +27,8 @@ class CreatePublisherAndPriceColumn extends Migration
                     \DB::table('books')->where('id', $book->id)
                         ->where('user_id', $book->user_id)
                         ->update([
-                            'publisher' => $query['publisher'],
-                            'price' => $query['price'],
+                            'publisher' => $query['publisher'] ?? null,
+                            'price' => $query['price'] ?? null,
                         ]);
                     echo "Updated: {$book->user_id} -> {$book->title}\n";
                 }
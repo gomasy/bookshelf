cf58ac6a3f1952821144568348d699e3fa4043f9
Gomasy <nyan@gomasy.jp>
2018-11-15 21:27:13 +0900

    プレビューモーダルの不可視領域を削除、CSS 修正

--- resources/assets/js/components/modals/previewModal.vue
+++ resources/assets/js/components/modals/previewModal.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="modal fade" id="preview-modal">
         <div class="modal-dialog">
-            <img :src="image">
+            <img :src="image" v-if="image !== null">
         </div>
     </div>
 </template>

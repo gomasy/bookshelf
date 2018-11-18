2884735a54cd697e5628b1928245c3b6ed55706c
Gomasy <nyan@gomasy.jp>
2018-11-18 13:46:13 +0900

    上流の変更を適用 (config/{logging,services}.php) (#82)
    * Add missing Mailgun 'endpoint' option
    
    Counterpart to https://github.com/laravel/framework/pull/25010
    
    * formatting
    
    * add level

--- config/logging.php
+++ config/logging.php
@@ -1,6 +1,7 @@
 <?php
 
 use Monolog\Handler\StreamHandler;
+use Monolog\Handler\SyslogUdpHandler;
 
 return [
 
@@ -60,6 +61,16 @@ return [
             'level' => 'error',
         ],
 
+        'papertrail' => [
+            'driver'  => 'monolog',
+            'level' => 'debug',
+            'handler' => SyslogUdpHandler::class,
+            'handler_with' => [
+                'host' => env('PAPERTRAIL_URL'),
+                'port' => env('PAPERTRAIL_PORT'),
+            ],
+        ],
+
         'stderr' => [
             'driver' => 'monolog',
             'handler' => StreamHandler::class,
@@ -76,15 +87,6 @@ return [
         'errorlog' => [
             'driver' => 'errorlog',
             'level' => 'debug',
-        ],
-
-        'papertrail' => [
-            'driver'  => 'monolog',
-            'handler' => Monolog\Handler\SyslogUdpHandler::class,
-            'handler_with' => [
-                'host' => env('PAPERTRAIL_URL'),
-                'port' => env('PAPERTRAIL_PORT'),
-            ],
         ],
     ],
 

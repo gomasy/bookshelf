gomasy/bookshelf

commit b8f4a9e4f956c7e57922e954391b869d19fbf3d4

reCAPTCHA 実装、テスト修正 (close #103)

app/Http/Controllers/Auth/LoginController.php
@@ -1,9 +1,13 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+
+use App\Http\Controllers\Controller;
+use App\Libs\ReCaptcha;
 
 class LoginController extends Controller
 {
@@ -18,7 +22,8 @@ class LoginController extends Controller
     |
     */
 
-    use AuthenticatesUsers;
+    use AuthenticatesUsers { attemptLogin as parentAttemptLogin; }
+    use ReCaptcha;
 
     /**
      * Where to redirect users after login.
@@ -35,5 +40,20 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Attempt to log the user into the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    protected function attemptLogin(Request $request): object
+    {
+        if (!$this->validateReCaptcha($request)) {
+            $this->reCaptchaFailed();
+        }
+
+        return $this->parentAttemptLogin($request);
     }
 }

app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Http\Controllers\Auth;
 

app/Http/Controllers/Auth/RegisterController.php
@@ -1,13 +1,17 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
 use Carbon\Carbon;
 
+use App\Http\Controllers\Controller;
+use App\Libs\ReCaptcha;
 use App\Bookshelf;
 use App\User;
-use App\Http\Controllers\Controller;
 
 class RegisterController extends Controller
 {
@@ -22,7 +26,7 @@ class RegisterController extends Controller
     |
     */
 
-    use RegistersUsers;
+    use RegistersUsers, ReCaptcha;
 
     /**
      * Where to redirect users after registration.
@@ -42,12 +46,43 @@ class RegisterController extends Controller
     }
 
     /**
+     * Handle a registration request for the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request): object
+    {
+        $this->validator($request->all())->validate();
+
+        if (!app()->runningUnitTests() && !$this->validateReCaptcha($request)) {
+            $this->reCaptchaFailed();
+        }
+
+        event(new Registered($user = $this->create($request->all())));
+        $this->guard()->login($user);
+
+        return $this->registered($request, $user)
+                        ?: redirect($this->redirectPath());
+    }
+
+    /**
+     * Get the login username to be used by the controller.
+     *
+     * @return string
+     */
+    protected function username(): string
+    {
+        return 'name';
+    }
+
+    /**
      * Get a validator for an incoming registration request.
      *
      * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
-    protected function validator(array $data)
+    protected function validator(array $data): object
     {
         return \Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
@@ -62,7 +97,7 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \App\User
      */
-    protected function create(array $data)
+    protected function create(array $data): object
     {
         $user = User::create([
             'name' => $data['name'],

app/Http/Controllers/Auth/ResetPasswordController.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Http\Controllers\Auth;
 

app/Http/Controllers/Auth/VerificationController.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Http\Controllers\Auth;
 

app/Http/Controllers/HomeController.php
@@ -5,8 +5,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Libs\ReCaptcha;
+
 class HomeController extends Controller
 {
+    use ReCaptcha;
+
+    protected function username(): string
+    {
+        return 'name';
+    }
+
     /**
      * Show the application dashboard.
      *
@@ -47,6 +56,10 @@ class HomeController extends Controller
 
     public function contact_submit(Request $request): object
     {
+        if (!app()->runningUnitTests() && !$this->validateReCaptcha($request)) {
+            $this->reCaptchaFailed();
+        }
+
         \Mail::send([], [], function ($message) use ($request) {
             $message->from($request->email)
                 ->to(\Config::get('mail.from.address'))

app/Libs/ReCaptcha.php
@@ -1,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Libs;
+
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\Request;
+
+trait ReCaptcha
+{
+    protected function validateReCaptcha(Request $request): bool
+    {
+        $recaptcha = new \ReCaptcha\ReCaptcha(\Config::get('recaptcha.secret_key'));
+
+        return $recaptcha->setExpectedHostname($request->getHost())
+            ->verify($request['g-recaptcha-response'], $request->ip())
+            ->isSuccess();
+    }
+
+    protected function reCaptchaFailed(): void
+    {
+        throw ValidationException::withMessages([
+            $this->username() => [ __('auth.recaptcha.failed') ],
+        ]);
+    }
+}

config/recaptcha.php
@@ -1,0 +1,6 @@
+<?php
+
+return [
+    'site_key' => env('RECAPTCHA_SITE_KEY'),
+    'secret_key' => env('RECAPTCHA_SECRET_KEY'),
+];

commit edb14b34fdb58e9b0996d7dfd94b8b8e1991bf09

ログインできない問題を修正 (caused by b8f4a9e4f956c7e57922e954391b869d19fbf3d4)

app/Http/Controllers/Auth/LoginController.php
@@ -48,7 +48,7 @@ class LoginController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return bool
      */
-    protected function attemptLogin(Request $request): object
+    protected function attemptLogin(Request $request): bool
     {
         if (!$this->validateReCaptcha($request)) {
             $this->reCaptchaFailed();
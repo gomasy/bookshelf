gomasy/bookshelf

commit f5a42f81f0b0eec3c923468cb66248c2d3a173b4

フリーワード検索、バックエンド側実装 (#74)

app/Http/Controllers/BookController.php
@@ -31,9 +31,13 @@ class BookController extends Controller
      *
      * @return array
      */
-    protected function appendHeader(array $book, int $sid): array
+    protected function appendHeader(array $book, int $sid): object
     {
-        return array_merge([ 'bookshelf_id' => $sid ], $book);
+        $book = new Book($book);
+        $book->bookshelf_id = $sid;
+        $book->status_id = 1;
+
+        return $book;
     }
 
     /**
@@ -127,23 +131,28 @@ class BookController extends Controller
      * @param Request $request
      * @param return Book|Response
      */
-    public function create(Request $request): object
+    public function create(Request $request): array
     {
         $this->checkAuthorize($request);
 
         $sid = $request->sid ?? Bookshelf::default()->id;
-        $book = \Cache::get($request->id);
-        if ($book) {
-            try {
-                Book::create($this->appendHeader($book, $sid));
+        $books = $request->toArray();
+        if (!count($books)) {
+            abort(400);
+        }
 
-                return response($book);
+        return \DB::transaction(function () use ($books, $sid) {
+            try {
+                for ($i = 0; $i < count($books); $i++) {
+                    $books[$i] = $this->appendHeader($books[$i], $sid);
+                    $books[$i]->save();
+                }
+
+                return $books;
             } catch (QueryException $e) {
                 abort(500);
             }
-        }
-
-        abort(400);
+        });
     }
 
     /**
@@ -173,20 +182,14 @@ class BookController extends Controller
     public function fetch(FetchRequest $request): object
     {
         $this->checkAuthorize($request);
+        $books = \NDL::query($request->code ?? $request->input, $request->type);
 
-        $book = new Book(\NDL::query($request->code));
-        if ($book->title !== null) {
-            $book->bookshelf_id = (int)$request->sid;
-            $book->status_id = 1;
-
-            if ($this->checkConflict($request, $book)) {
-                return response($book, 409);
+        if (count($books)) {
+            if (isset($request->code) && $this->checkConflict($request, $books[0])) {
+                return response($books, 409);
             }
 
-            $cid = md5($book->isbn . $book->jpno);
-            \Cache::put($cid, $book->toArray(), 5);
-
-            return response($book)->header('X-Request-Id', $cid);
+            return response($books);
         }
 
         abort(404);
@@ -224,15 +227,16 @@ class BookController extends Controller
     {
         $this->checkAuthorize($request);
 
-        \DB::beginTransaction();
-        try {
-            if (!Book::destroy($request->ids)) {
-                return response(\DB::rollback(), 400);
+        \DB::transaction(function () use ($request) {
+            try {
+                if (!Book::destroy($request->ids)) {
+                    abort(400);
+                }
+            } catch (QueryException $e) {
+                abort(500);
             }
+        });
 
-            return response(\DB::commit(), 204);
-        } catch (QueryException $e) {
-            return response(\DB::rollback(), 500);
-        }
+        return response(null, 204);
     }
 }

app/Libs/NDL.php
@@ -5,6 +5,8 @@ namespace App\Libs;
 
 use \GuzzleHttp\Exception\ConnectException;
 
+use \App\Book;
+
 class NDL
 {
     protected $endpoint = 'http://iss.ndl.go.jp/api/opensearch';
@@ -13,77 +15,85 @@ class NDL
         '/(.+?) \[?(著?|共著?)\]?/',
     ];
     protected $client;
+    protected $type;
     protected $obj;
 
     public function __construct()
     {
         $this->client = new \GuzzleHttp\Client([
             'base_uri' => $this->endpoint,
-            'timeout' => 1,
         ]);
     }
 
-    public function query(string $code): array
+    public function query(string $input, string $type): array
     {
-        $this->obj = $this->getItem($code);
-        if ($this->obj === null) {
-            return [];
+        $items = [];
+        $this->type = $type;
+
+        foreach ($this->getItems($input) as $obj) {
+            $this->obj = $obj;
+
+            array_push($items, new Book([
+                'title'     => $this->getTitle(),
+                'volume'    => $this->getVolume(),
+                'authors'   => $this->getAuthors(),
+                'isbn'      => $this->getISBN(),
+                'jpno'      => $this->getJPNO(),
+                'publisher' => $this->getPublisher(),
+                'price'     => $this->getPrice(),
+                'ndl_url'   => $this->getBookUrl(),
+            ]));
         }
 
-        return [
-            'title'     => $this->getTitle(),
-            'volume'    => $this->getVolume(),
-            'authors'   => $this->getAuthors(),
-            'isbn'      => $this->getISBN(),
-            'jpno'      => $this->getJPNO(),
-            'publisher' => $this->getPublisher(),
-            'price'     => $this->getPrice(),
-            'ndl_url'   => $this->getBookUrl(),
-        ];
+        return $items;
     }
 
-    protected function getQueryString(string $code): string
+    protected function getQueryString(string $input): string
     {
-        return '?' . http_build_query([
-            $this->searchType($code) => $code,
-        ]);
+        if ($this->type === 'code') {
+            $this->type = $this->searchType($input);
+        }
+
+        return '?' . http_build_query([ $this->type => $input ]);
     }
 
-    protected function getChannel(string $path): ?object
+    protected function getContent(string $path): string
     {
-        $content = null;
         $retry = 0;
 
         while ($retry <= 3) {
             try {
-                $content = (string)$this->client->request('GET', $path)->getBody();
-                break;
+                return (string)$this->client
+                    ->request('GET', $path, [ 'timeout' => $this->type !== 'title' ? 1 : 30 ])
+                    ->getBody();
             } catch (ConnectException $e) {
                 $retry++;
             }
         }
 
-        if ($content !== null) {
-            $xml = preg_replace($this->regexp[0], '', $content);
-
-            return simplexml_load_string($xml)->channel;
-        } else {
-            throw new \Exception('Retry limit reached');
-        }
-
-        return null;
+        throw new \Exception('Retry limit reached');
     }
 
-    public function getItem(string $code): ?object
+    protected function getChannel(string $path): object
     {
-        $channel = $this->getChannel($this->getQueryString($code));
+        $content = $this->getContent($path);
+        $xml = preg_replace($this->regexp[0], '', $content);
+
+        return simplexml_load_string($xml)->channel;
+    }
+
+    public function getItems(string $input): array
+    {
+        $channel = $this->getChannel($this->getQueryString($input));
+        $items = [];
         for ($i = 0; $i < $channel->totalResults; $i++) {
-            if ((string)$channel->item[$i]->category !== '障害者向け資料' && isset($channel->item[$i]->pubDate)) {
-                return $channel->item[$i];
+            $item = $channel->item[$i];
+            if (isset($item->category) && (string)$item->category !== '障害者向け資料' && isset($item->pubDate)) {
+                array_push($items, $channel->item[$i]);
             }
         }
 
-        return null;
+        return $items;
     }
 
     public function getISBN(): ?string
@@ -154,8 +164,6 @@ class NDL
                 return 'isbn';
             case 13:
                 return 'isbn';
-            default:
-                return 'any';
         }
     }
 

app/Http/Requests/BookFetchRequest.php
@@ -29,9 +29,8 @@ class BookFetchRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'code' => [
+            'input' => [
                 'required',
-                'regex:/^(.{8}|.{10}|.{13})$/',
                 new CorrectCheckDigit,
             ],
         ];

resources/lang/ja/validation.php
@@ -151,9 +151,6 @@ return [
         'password' => 'パスワード',
         'password-confirm' => 'パスワード確認',
 
-        // register book
-        'code' => '入力コード',
-
         // edit book
         'title' => 'タイトル',
         'volume' => '巻号',

commit b571e9e53fdcba3c1993688095f4de0a57d2b770

テストが壊れていたので修正 (caused by f5a42f81f0b0eec3c923468cb66248c2d3a173b4)

app/Http/Controllers/BookController.php
@@ -47,11 +47,20 @@ class BookController extends Controller
      * @param Book $book
      * @return bool
      */
-    protected function checkConflict(Request $request, Book $book): bool
+    protected function checkConflict(Request $request, array $books): array
     {
-        return (bool)Book::shelves($request->sid)->where(function ($query) use ($book) {
-            $query->where('isbn', $book->isbn)->orWhere('jpno', $book->jpno);
-        })->count();
+        $items = [];
+        foreach ($books as $book) {
+            $count = Book::shelves($request->sid)->where(function ($query) use ($book) {
+                $query->where('isbn', $book['isbn'])->orWhere('jpno', $book['jpno']);
+            })->count();
+
+            if ($count) {
+                array_push($items, $book);
+            }
+        }
+
+        return $items;
     }
 
     /**
@@ -138,7 +147,7 @@ class BookController extends Controller
         $sid = $request->sid ?? Bookshelf::default()->id;
         $books = $request->toArray();
         if (!count($books)) {
-            abort(400);
+            abort(400, 'Invalid request');
         }
 
         return \DB::transaction(function () use ($books, $sid) {
@@ -150,7 +159,7 @@ class BookController extends Controller
 
                 return $books;
             } catch (QueryException $e) {
-                abort(500);
+                abort(500, 'Query execution failed');
             }
         });
     }
@@ -182,17 +191,18 @@ class BookController extends Controller
     public function fetch(FetchRequest $request): object
     {
         $this->checkAuthorize($request);
-        $books = \NDL::query($request->code ?? $request->input, $request->type);
+        $books = \NDL::query($request->input, $request->type);
 
         if (count($books)) {
-            if (isset($request->code) && $this->checkConflict($request, $books[0])) {
-                return response($books, 409);
+            $items = $this->checkConflict($request, $books);
+            if (count($items)) {
+                return response($items, 409);
             }
 
             return response($books);
         }
 
-        abort(404);
+        abort(404, 'Book(s) not found');
     }
 
     /**
@@ -230,10 +240,10 @@ class BookController extends Controller
         \DB::transaction(function () use ($request) {
             try {
                 if (!Book::destroy($request->ids)) {
-                    abort(400);
+                    abort(400, 'Invalid request');
                 }
             } catch (QueryException $e) {
-                abort(500);
+                abort(500, 'Query execution failed');
             }
         });
 

app/Libs/NDL.php
@@ -33,7 +33,7 @@ class NDL
         foreach ($this->getItems($input) as $obj) {
             $this->obj = $obj;
 
-            array_push($items, new Book([
+            array_push($items, [
                 'title'     => $this->getTitle(),
                 'volume'    => $this->getVolume(),
                 'authors'   => $this->getAuthors(),
@@ -42,7 +42,7 @@ class NDL
                 'publisher' => $this->getPublisher(),
                 'price'     => $this->getPrice(),
                 'ndl_url'   => $this->getBookUrl(),
-            ]));
+            ]);
         }
 
         return $items;

database/migrations/2017_03_18_211154_create_books_table.php
@@ -21,7 +21,7 @@ class CreateBooksTable extends Migration
                   ->onDelete('cascade');
             $table->string('title');
             $table->string('volume')->nullable();
-            $table->string('authors');
+            $table->string('authors')->nullable();
             $table->char('isbn', 13)->nullable();
             $table->char('jpno', 10)->nullable();
             $table->date('published_date');

tests/Feature/BookTest.php
@@ -59,57 +59,53 @@ class BookTest extends TestCase
         $shelf = factory(Bookshelf::class)->create([ 'user_id' => $user->id ]);
 
         // success
-        $id = $this->actingAs($user)
-                   ->get("/fetch?sid={$shelf->id}&code=9784873115382", $headers)
-                   ->headers->get('X-Request-Id');
+        $books = $this->actingAs($user)
+                      ->get("/fetch?type=code&sid={$shelf->id}&input=9784873115382", $headers)
+                      ->original;
 
         $this->actingAs($user)
-             ->post('/create', [ 'id' => $id ], $headers)
+             ->post('/create', $books, $headers)
              ->assertSuccessful();
         $this->assertDatabaseHas('books', [ 'isbn' => '9784873115382' ]);
 
         // success (isbn10)
-        $id = $this->actingAs($user)
-                   ->get("/fetch?sid={$shelf->id}&code=4000801139", $headers)
-                   ->headers->get('X-Request-Id');
+        $books = $this->actingAs($user)
+                      ->get("/fetch?type=code&sid={$shelf->id}&input=4000801139", $headers)
+                      ->original;
 
         $this->actingAs($user)
-             ->post('/create', [ 'id' => $id ], $headers)
+             ->post('/create', $books, $headers)
              ->assertSuccessful();
         $this->assertDatabaseHas('books', [ 'isbn' => '9784000801133' ]);
 
         // success (jpno)
-        $id = $this->actingAs($user)
-                   ->get("/fetch?sid={$shelf->id}&code=22222222", $headers)
-                   ->headers->get('X-Request-Id');
+        $books = $this->actingAs($user)
+                      ->get("/fetch?type=code&sid={$shelf->id}&input=22222222", $headers)
+                      ->original;
 
         $this->actingAs($user)
-             ->post('/create', [ 'id' => $id ], $headers)
+             ->post('/create', $books, $headers)
              ->assertSuccessful();
         $this->assertDatabaseHas('books', [ 'jpno' => '22222222' ]);
 
         // dups
         $this->actingAs($user)
-             ->get("/fetch?sid={$shelf->id}&code=4873115388", $headers)
+             ->get("/fetch?type=code&sid={$shelf->id}&input=4873115388", $headers)
              ->assertStatus(409);
 
         // not found
         $this->actingAs($user)
-             ->get("/fetch?sid={$shelf->id}&code=1234567890128", $headers)
+             ->get("/fetch?type=code&sid={$shelf->id}&input=1234567890128", $headers)
              ->assertStatus(404);
 
         // invalid
         $this->actingAs($user)
-             ->get("/fetch?sid={$shelf->id}&code=1234567890123", $headers)
-             ->assertSessionHasErrors('code');
+             ->get("/fetch?type=code&sid={$shelf->id}&input=1234567890123", $headers)
+             ->assertSessionHasErrors('input');
 
         $this->actingAs($user)
-             ->get("/fetch?sid={$shelf->id}&code=", $headers)
-             ->assertSessionHasErrors('code');
-
-        $this->actingAs($user)
-             ->post('/create', [ 'X-Request-Id' => '' ], $headers)
-             ->assertStatus(400);
+             ->get("/fetch?type=code&sid={$shelf->id}&input=", $headers)
+             ->assertSessionHasErrors('input');
     }
 
     public function testFetchImage()
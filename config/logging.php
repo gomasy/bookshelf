b35952c7aa370f393de1a9ecc1c60979783e5cc9
Taylor Otwell <taylor@laravel.com>
2019-01-28 08:37:39 -0600

    adjust name of configuration value

--- config/logging.php
+++ config/logging.php
@@ -36,7 +36,6 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'lenient' => false,
             'channels' => ['daily'],
             'ignore_exceptions' => false,
         ],

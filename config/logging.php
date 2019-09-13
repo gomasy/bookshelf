cdc937c2ffd00f04e275ec3b33555149359e5873
Roger Vilà <rogervila@me.com>
2019-09-13 21:47:34 +0200

    [6.x] Add 'null' logging channel (#5106)
    * Add 'none' logging channel
    
    * Remove extra spaces
    
    * Rename 'none' channel to 'null'
    
    * Update logging.php

--- config/logging.php
+++ config/logging.php
@@ -1,5 +1,6 @@
 <?php
 
+use Monolog\Handler\NullHandler;
 use Monolog\Handler\StreamHandler;
 use Monolog\Handler\SyslogUdpHandler;
 
@@ -89,6 +90,11 @@ return [
         'errorlog' => [
             'driver' => 'errorlog',
             'level' => 'debug',
+        ],
+
+        'null' => [
+            'driver' => 'monolog',
+            'handler' => NullHandler::class,
         ],
     ],
 

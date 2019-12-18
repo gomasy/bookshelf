a1ec98ea4d4fa87ba9d738363185d2c659aabe5e
Taylor Otwell <taylor@laravel.com>
2019-12-18 13:44:16 -0600

    Utilize Authentication Middleware Contract  (#5181)
    * adjust auth middleware to point to contract
    
    * remove middleware priority

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Gate;
+use App\Http\Middleware\Authenticate;
+use Illuminate\Contracts\Auth\Middleware\AuthenticatesRequests;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -15,6 +17,16 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         // 'App\Model' => 'App\Policies\ModelPolicy',
     ];
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(AuthenticatesRequests::class, Authenticate::class);
+    }
 
     /**
      * Register any authentication / authorization services.

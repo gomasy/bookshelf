02c48fa1aef67e864332b553443102037a2a01a0
Taylor Otwell <taylor@laravel.com>
2019-12-20 14:15:04 -0600

    Apply fixes from StyleCI (#5186)

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -14,7 +14,7 @@ use Faker\Generator as Faker;
 |
 */
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+/* @var \Illuminate\Database\Eloquent\Factory $factory */
 
 $factory->define(\App\User::class, function (Faker $faker) {
     return [

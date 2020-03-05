6055700ea3bdc772858be215ec26c6847b6523b1
Gomasy <nyan@gomasy.jp>
2020-03-05 20:27:32 +0900

    Add .browserslistrc

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,6 @@
 module.exports = {
     plugins: {
         'autoprefixer': {
-            browsers: [
-                'last 2 versions',
-                'not dead',
-                'not op_mini all',
-                'not android <= 4.4.4',
-            ],
             grid: true,
         },
         'postcss-custom-media': {},
